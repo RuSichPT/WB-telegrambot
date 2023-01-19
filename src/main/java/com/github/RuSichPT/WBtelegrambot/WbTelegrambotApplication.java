aaed7325de97f6222a55c0fff86c2c45109eebfd
pavel.tokarev <pavel.tokarev@mail.ntc51>
2023-01-19 14:33:10 +0300

    Add sring boot app

---
+++ src/main/java/com/github/RuSichPT/WBtelegrambot/WbTelegrambotApplication.java
@@ -0,0 +1,13 @@
+package com.github.RuSichPT.WBtelegrambot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WbTelegrambotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WbTelegrambotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/RuSichPT/WBtelegrambot/WbTelegrambotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.RuSichPT.WBtelegrambot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WbTelegrambotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
