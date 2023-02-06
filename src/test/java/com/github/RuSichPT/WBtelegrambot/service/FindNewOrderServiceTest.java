e7ed2388ddddb90a5696057ca3a76101efffb23d
pavel.tokarev <pavel.tokarev@mail.ntc51>
2023-02-06 14:24:07 +0300

    Fix bug in job 2; NumNewOrders were not resetting

--- src/test/java/com/github/RuSichPT/WBtelegrambot/service/FindNewOrderServiceTest.java
+++ src/test/java/com/github/RuSichPT/WBtelegrambot/service/FindNewOrderServiceTest.java
@@ -5,12 +5,14 @@ import com.github.RuSichPT.WBtelegrambot.wbclient.WbClientPricesImpl;
 import com.github.RuSichPT.WBtelegrambot.wbclient.dto.Order;
 import com.github.RuSichPT.WBtelegrambot.wbclient.dto.Orders;
 import kong.unirest.HttpResponse;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.times;
 
 public class FindNewOrderServiceTest {
@@ -27,16 +29,19 @@ public class FindNewOrderServiceTest {
     private final Long chatId = 12564L;
     private final int NUM_USERS = 3;
 
-    @Test
-    public void shouldCorrectlyFindNewOrders() {
-        //given
-        String message = FindNewOrderServiceImpl.MESSAGE;
+    @BeforeEach
+    public void init() {
         Mockito.when(wbClientPrices.getNewOrders()).thenReturn(httpResponse);
         Mockito.when(httpResponse.getBody()).thenReturn(orders);
         Mockito.when(orders.getOrders()).thenReturn(orderList);
-        Mockito.when(orderList.size()).thenReturn(1);
+    }
 
-        Mockito.when(telegramUserService.findAll()).thenReturn(createListTelegramUser());
+    @Test
+    public void shouldCorrectlyNotifyAllUsers() {
+        //given
+        String message = FindNewOrderServiceImpl.MESSAGE;
+        Mockito.when(orderList.size()).thenReturn(1);
+        Mockito.when(telegramUserService.findAll()).thenReturn(createListTelegramUser(0));
 
         //when
         findNewOrderService.findNewOrders();
@@ -45,13 +50,29 @@ public class FindNewOrderServiceTest {
         Mockito.verify(sendBotMessageService, times(NUM_USERS)).sendMessage(chatId, message);
     }
 
-    private List<TelegramUser> createListTelegramUser() {
+    @Test
+    public void shouldCorrectlyResetNumNewOrders() {
+        //given
+        List<TelegramUser> list = createListTelegramUser(2);
+        List<TelegramUser> newList = createListTelegramUser(0);
+        Mockito.when(orderList.size()).thenReturn(0);
+        Mockito.when(telegramUserService.findAll()).thenReturn(list);
+
+        //when
+        findNewOrderService.findNewOrders();
+
+        //then
+        Mockito.verify(sendBotMessageService, never()).sendMessage(Mockito.any(Long.class), Mockito.any(String.class));
+        Mockito.verify(telegramUserService, times(NUM_USERS)).saveUser(newList.get(0));
+    }
+
+    private List<TelegramUser> createListTelegramUser(int numNewOrders) {
 
         List<TelegramUser> list = new ArrayList<>();
         for (int i = 0; i < NUM_USERS; i++) {
             TelegramUser user = new TelegramUser();
             user.setChatId(12564L);
-            user.setNumNewOrders(0);
+            user.setNumNewOrders(numNewOrders);
             list.add(user);
         }
 
